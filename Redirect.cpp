c5ed3f5b824166d9aac0f2284a160a139b23806a
Akhil Binoy <92171031+akhil888binoy@users.noreply.github.com>
2023-04-09 12:08:41 +0530

    redirect traffic

--- Redirect.cpp
+++ Redirect.cpp
@@ -9,3 +9,41 @@ IPAddress ip(192, 168, 1, 177);
 IPAddress server(192, 168, 1, 100);
 int port = 8080;
 
+EthernetClient client;
+
+void setup() {
+  // initialize the serial communication
+  Serial.begin(9600);
+  
+  // initialize the Ethernet shield
+  if (Ethernet.begin(mac) == 0) {
+    Serial.println("Failed to configure Ethernet using DHCP");
+  }
+  else {
+    Serial.print("Ethernet connection established. IP address: ");
+    Serial.println(Ethernet.localIP());
+  }
+}
+
+void loop() {
+  // read the value from the water level sensor and check if it's above the threshold
+  // ...
+  
+  // if flood is detected, send a message to the traffic control system
+  if (waterLevel > threshold) {
+    if (client.connect(server, port)) {
+      String message = "Flood detected on road X. Water level: " + String(waterLevel) + ". Time: " + String(millis());
+      client.println("GET /redirect_traffic?message=" + message + " HTTP/1.1");
+      client.println("Host: " + String(server));
+      client.println("Connection: close");
+      client.println();
+      delay(1000);
+      client.stop();
+    }
+    else {
+      Serial.println("Connection to the traffic control system failed");
+    }
+    // delay for 1 minute before sending another message
+    delay(60000);
+  }
+}
